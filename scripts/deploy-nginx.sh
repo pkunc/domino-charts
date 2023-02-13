8789abacf35cb4cb4e9261e47173da575f6a4a96
Petr Kunc <petr.kunc@gmail.com>
2023-02-13 23:48:29 +0100

    Create Deploy nginx script

---
+++ scripts/deploy-nginx.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Add ingress-nginx repository to Helm
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm repo list
+
+# Deploy ingress-nginx to a dedicated namespace "ingress-nginx"
+helm install \
+  ingress-nginx ingress-nginx/ingress-nginx \
+  --namespace ingress-nginx \
+  --create-namespace
