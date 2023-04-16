5da4e4938008d1e9f3481cefa2b71f4e2a206146
Petr Kunc <petr.kunc@gmail.com>
2023-04-16 10:52:06 +0200

    Update Cert-manager script (CRDs)

--- scripts/deploy-cert-manager.sh
+++ scripts/deploy-cert-manager.sh
@@ -5,12 +5,10 @@ helm repo add jetstack https://charts.jetstack.io
 helm repo update
 helm repo list
 
-# Define new CRDs for cert-manager
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.crds.yaml
-
 # Deploy cert-manager to a dedicated namespace "cert-manager"
 helm install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --create-namespace \
-  --version v1.11.0
+  --version v1.11.0 \
+  --set installCRDs=true
